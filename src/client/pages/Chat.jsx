382cda18110c6343dc542f0e9e1ef0965e7babb3
mtruvo <115444201+mtruvo@users.noreply.github.com>
2025-03-27 14:20:05 +0800

    Unfunctional

---
+++ src/client/pages/Chat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default function ChatInquiries() {
+    return (
+        <>
+            <Navbar role="client" />
+            <div>Hello, this is Chat Client!</div>
+        </>
+    );
+}
